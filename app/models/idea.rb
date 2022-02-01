e6a819a7255dda208b4c5335a0d69831a18ce3c9
MiBBy-4 <maksim.762373maks@bk.ru>
2022-02-01 16:24:20 +0300

    feat: add validation for Idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,8 @@
 class Idea < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :description, presence: true, length: { maximum: 250 }
+  validates :problem, presence: true
+  validates :sphere, presence: true
+  validates :geo_focus, presence: true
+  validates :investor_requirements, presence: true
 end
