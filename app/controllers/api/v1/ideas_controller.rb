dffd500abc5ab926f88213cd9c4a50e5a3015b6c
MiBBy-4 <maksim.762373maks@bk.ru>
2022-02-18 17:57:28 +0300

    feat: add parameter for count number of views

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -41,6 +41,6 @@ class Api::V1::IdeasController < ApplicationController
     end
 
     def idea_params
-      params.require(:idea).permit(:name, :description, :problem, :sphere, :investor_requirements, :geo_focus, :customer_id)
+      params.require(:idea).permit(:name, :description, :problem, :sphere, :investor_requirements, :geo_focus, :customer_id, :views)
     end
 end
