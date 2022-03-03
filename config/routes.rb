c0928212552a05a87477102389da86bdafba9417
MiBBy-4 <maksim.762373maks@bk.ru>
2022-03-03 22:21:46 +0300

    fix: add id parameter in request

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :ideas
-      patch :publication_period_update, to: 'ideas#update_publication_period'
+      patch 'publication_period_update/:id', to: 'ideas#update_publication_period'
     end
   end
 end
