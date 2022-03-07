95a62c9c70706c59baf898a024aa713c7ab71e9b
MiBBy-4 <maksim.762373maks@bk.ru>
2022-03-07 11:51:52 +0300

    feat: add letter_opener gem and configs for it

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -34,6 +34,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
