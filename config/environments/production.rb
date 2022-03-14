2fd9b50e1c87cc091d5ec54aed681f735cc09961
MiBBy-4 <maksim.762373maks@bk.ru>
2022-03-14 15:43:18 +0300

    refactor: change domain for sessions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,5 @@
 require "active_support/core_ext/integer/time"
 
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: ENV['HEROKU_APP_LINK']
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: ENV['HEROKU_APP_LINK']
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: ENV['HEROKU_APP_DOMAIN']
 else
   Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
