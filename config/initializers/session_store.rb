2a3ffdc5c75f2f592f4900968531fcfe0e9dec65
MiBBy-4 <maksim.762373maks@bk.ru>
2022-03-14 15:15:32 +0300

    fix: change sessions production link

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "mib-authentication-app--api.herokuapp.com"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: ENV['HEROKU_APP_LINK']
 else
   Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
