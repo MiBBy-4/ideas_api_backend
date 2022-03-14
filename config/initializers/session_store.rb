9f589be1c08d14ca15a9e45bbee74a4e483ab0b6
MiBBy-4 <maksim.762373maks@bk.ru>
2022-03-14 17:26:23 +0300

    refactor: change configuration of session stor

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,1 @@
-if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: :all, same_site: :none, secure: :true, tld_lenght: 2
-else
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
-end
+Application.config.session_store :cookie_store, :key => '_sample_app_session' , :domain => :all
