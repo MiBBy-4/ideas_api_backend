f82074adc666102d9d788366728b7407dcd1dbd3
MiBBy-4 <maksim.762373maks@bk.ru>
2022-03-14 16:53:36 +0300

    fix: change configuration of sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,11 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, { 
+    key: "ideas_backend_app",
+    domain: :all,
+    same_site: :none,
+    secure: :true,
+    tld_lenght: 2
+  }
+else
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
