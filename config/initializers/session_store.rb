56da570cfbdc6e7678d32d881f14d8a2736944a6
MiBBy-4 <maksim.762373maks@bk.ru>
2022-03-14 16:59:35 +0300

    fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,7 @@
-if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, { 
-    key: "ideas_backend_app",
-    domain: :all,
-    same_site: :none,
-    secure: :true,
-    tld_lenght: 2
-  }
-else
-  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
-end
+Rails.application.config.session_store :cookie_store, {
+  :key => '_ideas_api',
+  :domain => :all,
+  :same_site => :none,
+  :secure => :true,
+  :tld_length => 2
+}
