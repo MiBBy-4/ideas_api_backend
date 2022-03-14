5a9b7c70e5fc2e3c88d58ae9cab96738ab2d08e8
MiBBy-4 <maksim.762373maks@bk.ru>
2022-03-14 19:03:46 +0300

    refactor: change config for all domains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,1 @@
-if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_ideas_api", domain: ENV['HEROKU_APP_DOMAIN']
-else
-  Rails.application.config.session_store :cookie_store, key: "_ideas_api"
-end
+ Rails.application.config.session_store :cookie_store, key: "_ideas_api", domain: :all
