f3f8710ffad6482246eca0fe8186f720756fc7a2
MiBBy-4 <maksim.762373maks@bk.ru>
2022-03-14 17:04:17 +0300

    refactor: change number of arguments in sessions config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,5 @@
-Rails.application.config.session_store :cookie_store, {
-  :key => '_ideas_api',
-  :domain => :all,
-  :same_site => :none,
-  :secure => :true,
-  :tld_length => 2
-}
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: :all, same_site: :none, secure: :true, tld_lenght: 2
+else
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
