0abc73f21110dd4404344d7b797546651d091d75
MiBBy-4 <maksim.762373maks@bk.ru>
2022-03-14 17:16:23 +0300

    feat: add configuration to the production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,9 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.session_store :cookie_store, key: '_interslice_session'
+  config.middleware.use ActionDispatch::Cookies
+  config.middleware.use config.session_store, config.session_options
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
