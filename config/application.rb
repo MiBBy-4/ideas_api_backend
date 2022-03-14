a83e07a147eadc6cb87a338173ff54fbdda2b86e
MiBBy-4 <maksim.762373maks@bk.ru>
2022-03-14 23:46:59 +0300

    feat: try to change config of application to set sessions

--- config/application.rb
+++ config/application.rb
@@ -25,15 +25,8 @@ module IdeasApiBackend
     config.load_defaults 6.1
 
     config.action_controller.default_protect_from_forgery = true
-
-    if Rails.env == 'production'
-      config.session_store :cookie_store, key: "_ideas_api", domain: ENV['HEROKU_APP_DOMAIN']
-    else
-      config.session_store :cookie_store, key: "_ideas_api"
-    end
-
+    config.session_store :cookie_store, key: "_ideas_api", :domain => :all, :same_site => :none, :secure => :true
     config.middleware.use ActionDispatch::Cookies
-    
     config.middleware.use config.session_store, config.session_options
     # Configuration for the application, engines, and railties goes here.
     #
